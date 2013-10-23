8dd9516a5a8ed76bafbbef8a408d0b8c788e706e
Marc <sanpeihawaii2@gmail.com>
2013-10-23 09:36:29 -1000

    a

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,14 +3,7 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Index;
-import views.html.KeliaMoniz;
 import views.html.Page1;
-import views.html.kalanid;
-import views.html.cmoore;
-import views.html.jakem;
-import views.html.souza;
-import views.html.kolohe;
-import views.html.KeliaMoniz;
 
 /**
  * Implements the controllers for this application.
@@ -34,58 +27,5 @@ public class Application extends Controller {
     
   }
   
-  /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
-   */
-  public static Result kalanid() {
-    return ok(kalanid.render("Welcome to Page1."));
-    
-  }
-  
-  /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
-   */
-  public static Result cmoore() {
-    return ok(cmoore.render("Welcome to Page1."));
-    
-  }
-  
-  /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
-   */
-  public static Result jakem() {
-    return ok(jakem.render("Welcome to Page1."));
-    
-  }
-  
-  /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
-   */
-  public static Result souza() {
-    return ok(souza.render("Welcome to Page1."));
-    
-  }
-  
-  /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
-   */
-  public static Result kolohe() {
-    return ok(kolohe.render("Welcome to Page1."));
-    
-  }
-  
-  /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
-   */
-  public static Result keliamoniz() {
-    return ok(KeliaMoniz.render("Welcome to Page1."));
-    
-  }
   
 }
